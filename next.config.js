39f105bfa7c531fc6a6a781b048ee5b8b3f961d5
Fredswqa190 <72878800+Fredswqa190@users.noreply.github.com>
2023-08-10 02:16:21 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    trailingSlash: true,
+    output: 'export',
+}
+
+/*async redirects() {
+      return [
+        {
+          source: '/about',
+          destination: '/',
+          permanent: true,
+        },
+      ]
+    },*/
+
+module.exports = nextConfig
