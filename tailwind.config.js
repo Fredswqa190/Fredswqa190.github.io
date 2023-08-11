28aeb52a539c28577592cbfa70921593f83a828a
Fredswqa190 <72878800+Fredswqa190@users.noreply.github.com>
2023-08-11 12:53:52 -0400

    fixing failwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,14 +18,14 @@ module.exports = {
     },
   },
   plugins: [
-    require('taos/plugin')
+    //require('taos/plugin')
   ],
-  safelist: [
+  /*safelist: [
     '!duration-[0ms]',
     '!delay-[0ms]',
     'html.js :where([class*="taos:"]:not(.taos-init))'
   ],
-  /*content: {
+  content: {
     relative: true,
     transform: (content) => content.replace(/taos:/g, ''),
     files: ['./src/*.{html,js}'],
