8dfc55c1a431fbee75265c43ffa34f80b89c20f2
Fredswqa190 <72878800+Fredswqa190@users.noreply.github.com>
2023-08-10 21:44:21 -0400

    modified tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,17 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('taos/plugin')
+  ],
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ['./src/*.{html,js}'],
+  },
 }
