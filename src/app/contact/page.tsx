775cb8fe72b671493615789b8cea9400e77d0bc3
Fredswqa190 <72878800+Fredswqa190@users.noreply.github.com>
2023-08-10 21:33:06 -0400

    contact page update for text

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,7 +11,7 @@ export default function Contact() {
           *
         </span>
       </label>
-      <input type="text" name="fullname" className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500" />
+      <input type="text" name="fullname" className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-50" />
 
       <label htmlFor="email" className="text-gray-500 font-light mt-4 dark:text-gray-50">
         E-mail
@@ -19,14 +19,14 @@ export default function Contact() {
           *
         </span>
       </label>
-      <input type="email" name="email" className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500" />
+      <input type="email" name="email" className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-50" />
 
       <label htmlFor="subject" className="text-gray-500 font-light mt-4 dark:text-gray-50">
         Subject
         <span className="text-red-500">
         *</span>
       </label>
-      <input type="text" name="subject" className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500" />
+      <input type="text" name="subject" className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-50" />
 
       <label htmlFor="message" className="text-gray-500 font-light mt-4 dark:text-gray-50">
         Message
@@ -34,7 +34,7 @@ export default function Contact() {
           *
         </span>
       </label>
-      <textarea name="message" className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"></textarea>
+      <textarea name="message" className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-50"></textarea>
       <div className="flex flex-row items-center justify-start">
         <button className="px-10 mt-8 py-2 bg-[#130F49] text-gray-50 font-light rounded-md text-lg flex flex-row items-center">
           Send
